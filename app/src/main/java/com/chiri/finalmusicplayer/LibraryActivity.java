256592b0ed6bd30c4d73a6683da7a244a136b379
chiri <chirichignoa@gmail.com>
2017-04-17 21:51:43 -0300

    Agregado de tabs

--- app/src/main/java/com/chiri/finalmusicplayer/LibraryActivity.java
+++ app/src/main/java/com/chiri/finalmusicplayer/LibraryActivity.java
@@ -1,13 +1,25 @@
 package com.chiri.finalmusicplayer;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 
 public class LibraryActivity extends AppCompatActivity {
+    ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_library);
+
+
+        TabLayout tabs = (TabLayout) findViewById(R.id.tabs);
+
+
+        tabs.addTab(tabs.newTab().setIcon(R.drawable.ic_music_note));
+        tabs.addTab(tabs.newTab().setIcon(R.drawable.ic_album));
+        tabs.addTab(tabs.newTab().setIcon(R.drawable.ic_playlist_play));
     }
+
 }
--- app/src/main/java/com/chiri/finalmusicplayer/MainActivity.java
+++ app/src/main/java/com/chiri/finalmusicplayer/MainActivity.java
@@ -1,13 +1,28 @@
 package com.chiri.finalmusicplayer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageButton libraryButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        libraryButton = (ImageButton)findViewById(R.id.libraryButton);
+        libraryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent libraryView = new Intent(MainActivity.this , LibraryActivity.class);
+                //libraryView.putExtra("Something", something);
+                startActivity(libraryView);
+            }
+        });
     }
 }
