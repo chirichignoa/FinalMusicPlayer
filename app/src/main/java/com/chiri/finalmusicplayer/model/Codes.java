67ad390bad04a58b3344787792baf6f9d8e0ef00
chiri <chirichignoa@gmail.com>
2017-05-27 00:55:03 -0300

    Clase estatica con codigos

---
+++ app/src/main/java/com/chiri/finalmusicplayer/model/Codes.java
@@ -0,0 +1,18 @@
+package com.chiri.finalmusicplayer.model;
+
+import android.provider.MediaStore;
+
+/**
+ * Created by chiri on 26/05/17.
+ */
+
+public final class Codes {
+    public final static String TAG_TYPE = "TYPE";
+    public final static String TAG_SONG = "SONG";
+    public final static String TAG_ALBUM = "ALBUM";
+    public final static String TAG_PLAYLIST = "PLAYLIST";
+    public final static String TAG_TITLE = MediaStore.Audio.Media.TITLE;
+    public final static String TAG_ARTIST = MediaStore.Audio.Media.ARTIST;
+    public final static String CODE_ADD_SONG_QUEUE = "ADD_SONG_QUEUE";
+
+}
