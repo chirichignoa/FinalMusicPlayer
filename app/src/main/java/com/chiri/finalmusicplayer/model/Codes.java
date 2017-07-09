92afff20482ee39b5eaa7f40f5cc6774c73b6237
chiri <chirichignoa@gmail.com>
2017-07-09 13:46:08 -0300

    Algunos codigos

--- app/src/main/java/com/chiri/finalmusicplayer/model/Codes.java
+++ app/src/main/java/com/chiri/finalmusicplayer/model/Codes.java
@@ -11,13 +11,21 @@ public final class Codes {
     public final static String TAG_SONG = "SONG";
     public final static String TAG_ALBUM = "ALBUM";
     public final static String TAG_PLAYLIST = "PLAYLIST";
+
     public final static String TAG_SONG_TITLE = MediaStore.Audio.Media.TITLE;
     public final static String TAG_ALBUM_TITLE = MediaStore.Audio.AlbumColumns.ALBUM;
     public final static String TAG_ALBUM_ARTIST = MediaStore.Audio.AlbumColumns.ARTIST;
     public final static String TAG_ARTIST = MediaStore.Audio.Media.ARTIST;
     public final static String TAG_ALBUMART = MediaStore.Audio.AlbumColumns.ALBUM_ART;
     public final static String TAG_PLAYLIST_NAME = MediaStore.Audio.Playlists.NAME;
+
     public final static String TAG_ADD_SONG_QUEUE = "ADD_SONG_QUEUE";
     public final static String TAG_SEND_RESULT = "SEND_RESULT";
+    public final static String TAG_SEND_CURRENT_PLAYLIST = "SEND_CURRENT_PLAYLIST";
+    public final static String TAG_CURRENT_PLAYLIST = "CURRENT_PLAYLIST";
+    public final static String TAG_NUMBER_SONG = "NUMBER_SONG";
+    public final static String TAG_NUMBER = "NUMBER";
+
+
 
 }
