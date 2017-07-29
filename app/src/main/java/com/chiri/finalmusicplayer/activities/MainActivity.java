b21519180ca81d46641385d768d978d292ae3a4d
chiri <chirichignoa@gmail.com>
2017-07-28 23:39:37 -0300

    Soporte de lyrics

--- app/src/main/java/com/chiri/finalmusicplayer/activities/MainActivity.java
+++ app/src/main/java/com/chiri/finalmusicplayer/activities/MainActivity.java
@@ -1,5 +1,6 @@
 package com.chiri.finalmusicplayer.activities;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.ComponentName;
@@ -7,28 +8,47 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Handler;
 import android.os.IBinder;
+import android.os.RemoteException;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.chiri.finalmusicplayer.R;
 import com.chiri.finalmusicplayer.adapters.CurrentPlayListAdapter;
 import com.chiri.finalmusicplayer.model.Codes;
 import com.chiri.finalmusicplayer.model.Song;
 import com.chiri.finalmusicplayer.service.MusicService;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -40,15 +60,28 @@ public class MainActivity extends AppCompatActivity {
     private int duration;
     private ImageButton playPause, nextSong, previousSong;
     private ImageView albumArt;
-    private TextView songName, artistName, totalTime, currentTime;
+    private TextView songName, artistName, totalTime, currentTime, lyricView;
     private SeekBar seekBar;
     private ListView currentPlayList;
     private CurrentPlayListAdapter adapter;
     private List<Song> songs = new ArrayList<>();
+    private Button lyricButton;
+
+    private View.OnClickListener hiddenItems, restoreItems;
 
     private static final int CODIGO_LibraryActivity = 1;
+
+    private static final String API_TOKEN = "32ccacf88fc879900f65b8784e7cf927";
+    private static final String API_URL = "http://api.vagalume.com.br/search.php?";
+    private static final String API_PARAMETER_SEPARATOR = "&";
+    private static final String API_PARAMETER_ARTIST = "art=";
+    private static final String API_PARAMETER_SONG = "mus=";
+    private static final String API_PARAMETER_KEY = "apikey=";
+
     private MusicService.MusicServiceBinder iCallService;
     private MusicService musicService;
+
+    private static final int PERMISSION_REQUEST_CODE = 1;
 
     private ServiceConnection sc = new ServiceConnection() {
         @Override
@@ -67,9 +100,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (checkPermission()) {
+            init();
+        } else {
+            requestPermission(); // Code for permission
+        }
 
-        init();
 
+
+    }
+
+    private void init(){
         ImageButton libraryButton = (ImageButton) findViewById(R.id.libraryButton);
         libraryButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -78,11 +119,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(libraryView, CODIGO_LibraryActivity);
             }
         });
-
-
-    }
-
-    private void init(){
         playPause = (ImageButton)findViewById(R.id.playButton);
         nextSong = (ImageButton)findViewById(R.id.nextSongButton);
         previousSong = (ImageButton)findViewById(R.id.previousSongButton);
@@ -151,6 +187,26 @@ public class MainActivity extends AppCompatActivity {
                     MainActivity.this.songs.addAll((ArrayList) intent.getExtras().getParcelableArrayList(Codes.TAG_CURRENT_PLAYLIST));
                     Log.i("Result-for-currentPL", MainActivity.this.songs.toString());
                 }
+            }
+        };
+        lyricView = (TextView) findViewById(R.id.lyricView);
+        lyricView.setVisibility(View.INVISIBLE);
+        lyricView.setMovementMethod(new ScrollingMovementMethod());
+        lyricButton = (Button)findViewById(R.id.lyricButton);
+        this.hiddenItems = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity.this.searchLyric();
+            }
+        };
+        lyricButton.setOnClickListener(this.hiddenItems);
+        this.restoreItems = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity.this.setItemsStatus(View.VISIBLE);
+                MainActivity.this.lyricView.setVisibility(View.INVISIBLE);
+                MainActivity.this.lyricButton.setText(R.string.lyricButton);
+                MainActivity.this.lyricButton.setOnClickListener(MainActivity.this.hiddenItems);
             }
         };
     }
@@ -262,4 +318,106 @@ public class MainActivity extends AppCompatActivity {
         LocalBroadcastManager.getInstance(this).unregisterReceiver(receiverCurrentPlaylist);
         super.onPause();
     }
+
+    private void searchLyric() {
+        if(playing){
+            this.setItemsStatus(View.INVISIBLE);
+            this.lyricView.setVisibility(View.VISIBLE);
+            this.lyricButton.setText(R.string.backButton);
+            this.lyricButton.setOnClickListener(this.restoreItems);
+            String artistName = this.artistName.getText().toString().replace(" ","-");
+            String songName = this.songName.getText().toString().replace(" ","-");
+
+            StringBuffer url = new StringBuffer();
+                url.append(API_URL)
+                .append(API_PARAMETER_ARTIST)
+                .append(artistName)
+                .append(API_PARAMETER_SEPARATOR)
+                .append(API_PARAMETER_SONG)
+                .append(songName)
+                .append(API_PARAMETER_SEPARATOR)
+                .append(API_PARAMETER_KEY)
+                .append(API_TOKEN);
+
+            // Instantiate the RequestQueue.
+            RequestQueue queue = Volley.newRequestQueue(this);
+
+            Log.d("URL", url.toString());
+            // Request a string response from the provided URL.
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url.toString(), null,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            lyricView.setText(parseJSON(response));
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    lyricView.setText(R.string.lyricError);
+                }
+            });
+            // Add the request to the RequestQueue.
+            queue.add(jsonObjectRequest);
+        } else {
+            Toast.makeText(getApplicationContext(), R.string.lyricMessage,Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private String parseJSON(JSONObject response)  {
+        JSONArray array = null;
+        try {
+            array = response.getJSONArray("mus");
+            if(array == null) {
+                return getApplicationContext().getString(R.string.lyricNotFound);
+            } else {
+                    return response.getJSONArray("mus").getJSONObject(0).getString("text");
+            }
+        }
+        catch (JSONException e) {
+            return getApplicationContext().getString(R.string.lyricNotFound);
+        }
+    }
+
+    private boolean checkPermission() {
+        int result = ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.INTERNET);
+        if (result == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private void requestPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.INTERNET)) {
+            Toast.makeText(MainActivity.this, "Usar internet permite obtener letras de las canciones. Por favor conceda este permiso en los ajustes.", Toast.LENGTH_LONG).show();
+        } else {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{android.Manifest.permission.INTERNET}, PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.e("value", "Permission Granted.");
+                    init();
+                } else {
+                    Log.e("value", "Permission Denied.");
+                }
+                break;
+        }
+    }
+
+    private void setItemsStatus(int visibility) {
+        this.playPause.setVisibility(visibility);
+        this.nextSong.setVisibility(visibility);
+        this.previousSong.setVisibility(visibility);
+        this.totalTime.setVisibility(visibility);
+        this.currentTime.setVisibility(visibility);
+        this.seekBar.setVisibility(visibility);;
+        this.currentPlayList.setVisibility(visibility);
+    }
+
+
 }
